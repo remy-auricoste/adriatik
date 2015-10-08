047b5916530c3cfd933c02c11f905d33f4131acd
remy <remy.auricoste@gmail.com>
2015-10-08 13:29:11 +0200

    config server for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+var static = require('node-static');
+
+var port = process.env.PORT || 8000;
+
+var fileServer = new static.Server('./src', {
+    cache: 1
+});
+
+String.prototype.startsWith = function(start) {
+    return start.length <= this.length && this.substring(0, start.length) === start;
+}
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+      fileServer.serve(request, response);
+    }).resume();
+}).listen(port);
