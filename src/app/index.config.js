9e46f9be32a799f3c4b3fb3408cd9ef0ba3dd06a
remy <remy.auricoste@gmail.com>
2015-10-08 14:17:10 +0200

    clean useless config + remove dist dir

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,16 +6,11 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig) {
+  function config($logProvider, $locationProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
 
-    // Set options third-party lib
-    toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
-    toastrConfig.preventDuplicates = true;
-    toastrConfig.progressBar = true;
+    $locationProvider.html5Mode(true);
   }
 
 })();
