85010e61b51be493aa24f8540e86a2964a2c3c3d
alain DOAN-HUU <adoanhuu@gmail.com>
2015-10-21 14:02:54 +0200

    refactor bid panel

--- src/app/components/bidPanel/bidPanel.js
+++ src/app/components/bidPanel/bidPanel.js
@@ -10,42 +10,23 @@ function bidPanel() {
               game: "="
             },
             link: function(scope, elements, attr) {
-              scope.onGodClick = function(god) {
-                if (scope.game.phase !== Phases.bidding) {
-                  return;
-                }
-                if (god === God.Apollon) {
-                  scope.game.placeBid(scope.game.currentPlayer, god, 0);
-                  return;
-                }
-                scope.god = god;
-                scope.customBidValue = god.bid ? (god.bid.gold+1) : 1;
-              }
-              scope.getBidValues = function() {
-                if (!scope.god) {
-                  return [];
-                }
-                var startValue = scope.god.bid ? scope.god.bid.gold+1 : 1;
-                var result = [];
-                for (var i=0;i<5;i++) {
-                  result.push(startValue + i);
-                }
-                return result;
+              scope.counters = [];
+              for (var i=1;i<=20;i++) {
+                  scope.counters.push(i);
               }
               scope.noBidPlayers = function() {
                 return scope.game.players.filter(function(player) {
                   return !player.bid;
                 })
               }
-              scope.placeBid = function(value) {
-                var removedPlayer = scope.game.placeBid(scope.game.currentPlayer, scope.god, value);
+              scope.placeBid = function(god, value) {
+                var removedPlayer = scope.game.placeBid(scope.game.currentPlayer, god, value);
                 console.log("removed player", removedPlayer);
                 console.log("bidding", scope.game.currentPlayer);
-                scope.god = null;
               }
-              scope.placeCustomBid = function() {
+              scope.placeCustomBid = function(god) {
                 var amount = parseInt(scope.customBidValue);
-                scope.placeBid(amount);
+                scope.placeBid(god, amount);
               }
               scope.endTurn = function() {
                 scope.game.endPlayerTurn();
