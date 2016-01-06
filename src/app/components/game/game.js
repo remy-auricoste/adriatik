5c25a6217b85b89df7201c783846a41a2a87af81
remy <remy.auricoste@gmail.com>
2016-01-06 09:48:04 +0100

    execute command locally as soon as it is clicked

--- src/app/components/game/game.js
+++ src/app/components/game/game.js
@@ -23,27 +23,37 @@ function game(gameInitializer, $route, randomFactory, qPlus, gameStorage, $rootS
               console.error(err);
             });
 
+            var executeCommand = function(command) {
+                var id = Math.random() + "";
+                command.id = id;
+                randomFactory.setGlobalId(command.id);
+                var result = scope.game.receiveCommand(command);
+                var thenFct = function(result) {
+                  gameStorage.save(scope.game);
+                }
+                if (result && typeof result.then === "function") {
+                  result.then(thenFct).catch(function(err) {
+                    console.error("error");
+                    console.error(err);
+                  });
+                } else {
+                  thenFct(result);
+                }
+            }
+
             commandSocket.addListener(function(messageObj) {
               var source = messageObj.source;
+              if (source === commandSocket.getId()) {
+                // ignore my commands
+                return;
+              }
               var command = Command.fromObject(messageObj.message);
               console.log("commandSocket received", command, "source", source);
               var playerSocketId = command.player.account.id;
               if (!config.isDev() && playerSocketId !== source) {
                 throw new Error("received a command not from the actual player. source="+source+". player id="+playerSocketId);
               }
-              randomFactory.setGlobalId(command.id);
-              var result = scope.game.receiveCommand(command);
-              var thenFct = function(result) {
-                gameStorage.save(scope.game);
-              }
-              if (result && typeof result.then === "function") {
-                result.then(thenFct).catch(function(err) {
-                  console.error("error");
-                  console.error(err);
-                });
-              } else {
-                thenFct(result);
-              }
+              executeCommand(command);
               scope.$apply();
             });
 
@@ -51,9 +61,7 @@ function game(gameInitializer, $route, randomFactory, qPlus, gameStorage, $rootS
               if (!config.isDev() && commandSocket.getId() !== command.player.account.id) {
                 throw new Error("Ce n'est pas à votre tour de jouer");
               }
-
-              var id = Math.random() + "";
-              command.id = id;
+              executeCommand(command);
               console.log("sending command", command);
               commandSocket.send(JSON.parse(JSON.stringify(command)));
             });
