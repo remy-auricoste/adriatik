8fdd6a8993c1b1f45bb1db5bc51ed0c2723f60ec
remy <remy.auricoste@gmail.com>
2016-04-17 22:50:16 +0200

    attempt to fix deployment on heroku

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 var express = require('express');
 var app = express();
-var urlTool = require("url");
 
 var port = process.env.PORT || 3000;
-
-
-String.prototype.startsWith = function(start) {
-    return start.length <= this.length && this.substring(0, start.length) === start;
-}
 
 app.use("/", express.static("dist"));
 app.use("/", express.static("src"));
 
 var server = app.listen(port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+//  var host = server.address().address;
+//  var port = server.address().port;
+//
+//  console.log('Example app listening at http://%s:%s', host, port);
 });
