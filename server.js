24504b3476ff008454d6a57a76e7497026bba67a
remy <remy.auricoste@gmail.com>
2016-04-17 23:07:09 +0200

    attempt to fix deployment on heroku, using node-static instead of express

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-var express = require('express');
-var app = express();
+var static = require('node-static');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
-app.use("/", express.static("dist"));
-app.use("/", express.static("src"));
-
-var server = app.listen(port, function () {
-//  var host = server.address().address;
-//  var port = server.address().port;
-//
-//  console.log('Example app listening at http://%s:%s', host, port);
+var fileServer = new static.Server('./src', {
+    cache: 1
 });
+var distServer = new static.Server('./dist', {
+    cache: 1
+});
+
+String.prototype.startsWith = function(start) {
+    return start.length <= this.length && this.substring(0, start.length) === start;
+}
+String.prototype.endsWith = function(end) {
+    return end.length <= this.length && this.substring(this.length-end.length) === end;
+}
+
+console.log("creating server listening on port", port);
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        if (request.url.endsWith(".css") || request.url.endsWith(".js")) {
+            distServer.serve(request, response);
+        } else {
+            fileServer.serve(request, response);
+        }
+    }).resume();
+}).listen(port);
