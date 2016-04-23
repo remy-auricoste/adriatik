7dae5050dc9ad10e428d48ddf3d5503b7af138a1
remy <remy.auricoste@gmail.com>
2016-04-23 17:08:42 +0200

    [battle-panel] Battle : refactor object to get access to intermediate battle scores

--- src/model/data/Battle.js
+++ src/model/data/Battle.js
@@ -4,29 +4,38 @@ var Dice = require("./Dice");
 var Battle = Meta.createClass("Battle", {
   randoms: [],
   territory: "Territory",
+  init: function() {
+    var self = this;
+    var player1 = this.territory.units[0].owner;
+    this.player1 = player1;
+    this.player2 = this.territory.units.filter(function (unit) {
+      return unit.owner !== player1;
+    })[0].owner;
+    this.players = [this.player1, this.player2];
+    this.dices = this.randoms.map(Dice);
+    this.strengths = this.players.map(function(player) {
+      return self.territory.getUnits(player).length;
+    });
+    this.scores = this.strengths.map(function(strength, index) {
+      // TODO count defensive buildings
+      return strength + self.dices[index];
+    });
+  },
   getDices: function() {
-    return this.randoms.map(Dice);
+    return this.dices;
   },
   getLoosers: function() {
     var territory = this.territory;
-    var owner1 = territory.units[0].owner;
-    var owner2 = territory.units.filter(function (unit) {
-        return unit.owner !== owner1;
-    })[0].owner;
-    var getStrength = function (owner, randomValue) {
-        // TODO count defensive buildings
-        return territory.getUnits(owner).length + Dice(randomValue);
-    }
-    var strength1 = getStrength(owner1, this.randoms[0]);
-    var strength2 = getStrength(owner2, this.randoms[1]);
+    var strength1 = this.strengths[0];
+    var strength2 = this.strengths[1];
     var loss1 = strength1 <= strength2 ? 1 : 0;
     var loss2 = strength2 <= strength1 ? 1 : 0;
     var result = [];
     if (loss1) {
-      result.push(owner1);
+      result.push(this.player1);
     }
     if (loss2) {
-      result.push(owner2);
+      result.push(this.player2);
     }
     return result;
   }
