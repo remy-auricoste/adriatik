739b1d723859e438f2f176bd60d29b67a7022909
remy <remy.auricoste@gmail.com>
2016-05-25 09:58:39 +0200

    log prod logs onto log-collector

--- src/alias/Logger.js
+++ src/alias/Logger.js
@@ -1,4 +1,14 @@
+var Request = require("rauricoste-request");
+var config = require("../services/config");
+
 var rootLogger = require("rauricoste-logger");
 rootLogger.setLevel("info");
+if (!config.isLocal()) {
+  rootLogger.addAppender(function(logger, methodName, callArgs) {
+    if (methodName === rootLogger.levels.error.name) {
+      new Request().post("http://log-collector.herokuapp.com", [logger.name, methodName].concat(callArgs).join("  "));
+    }
+  });
+}
 
 module.exports = rootLogger;
--- src/tests.js
+++ src/tests.js
@@ -1,12 +1,13 @@
+require("./test/window");
+
+var env = require("./test/env");
+env.mode = "test";
+
 var logger = require("./alias/Logger").getLogger("tests");
 logger.info("loading tests.js");
 
 Meta = require("rauricoste-meta");
 
-var env = require("./test/env");
-env.mode = "test";
-
-require("./test/window");
 require("./model/natif/Arrays");
 require("./model/natif/Errors");
 require("./model/natif/Strings");
