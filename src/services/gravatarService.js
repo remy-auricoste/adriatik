f629950e6aea38d7749049f1ae983ebb26ba585a
remy <remy.auricoste@gmail.com>
2017-05-06 07:46:16 +0200

    fix gravatarService

--- src/services/gravatarService.js
+++ src/services/gravatarService.js
@@ -1,43 +1,39 @@
 var Request = require("rauricoste-request");
 var logger = require("../alias/Logger").getLogger("gravatarService");
+var md5 = require("MD5");
 
-/** @ngInject */
-function gravatarService(md5, $http) {
-  'use strict';
-
-  return {
-    cache: {},
-    getJson: function(email) {
-      if (this.cache[email]) {
-        return this.cache[email];
-      }
-      var hash = md5.createHash(email);
-      return new Request().get("https://www.gravatar.com/"+hash).then(function(res) {
-        logger.info(res.headers);
-        var locationHeader = res.headers.Location;
-        if (locationHeader) {
-          return new Request().get(locationHeader);
-        } else {
-          return res;
-        }
-      }).then(function(res) {
-        logger.info(res.body);
-        return JSON.parse(res.body);
-      });
-    },
-    getUsername: function(email) {
-      return this.getJson(email).then(function(gravatar) {
-        logger.info(gravatar);
-      });
-    },
-    getPictureUrl: function(email) {
-      if (!email) {
-        return null;
-      }
-      return "https://www.gravatar.com/avatar/"+md5.createHash(email)+"?s=50";
+var gravatarService = {
+  cache: {},
+  getJson: function(email) {
+    if (this.cache[email]) {
+      return this.cache[email];
     }
+    var hash = md5.createHash(email);
+    return new Request().get("https://www.gravatar.com/"+hash).then(function(res) {
+      logger.info(res.headers);
+      var locationHeader = res.headers.Location;
+      if (locationHeader) {
+        return new Request().get(locationHeader);
+      } else {
+        return res;
+      }
+    }).then(function(res) {
+      logger.info(res.body);
+      return JSON.parse(res.body);
+    });
+  },
+  getUsername: function(email) {
+    return this.getJson(email).then(function(gravatar) {
+      logger.info(gravatar);
+    });
+  },
+  getPictureUrl: function(email) {
+    if (!email) {
+      return null;
+    }
+    return "https://www.gravatar.com/avatar/"+md5.createHash(email)+"?s=50";
   }
 }
-angular.module("adriatik").service("gravatarService", gravatarService);
 
+module.exports = gravatarService;
 
