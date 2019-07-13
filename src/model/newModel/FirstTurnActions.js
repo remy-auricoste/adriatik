347d8f3a5644f55ee13ef1ace7e714568a8f8419
remy <remy.auricoste@gmail.com>
2019-07-13 16:32:22 +0200

    fix a few bugs + start test on several turns

--- src/model/newModel/FirstTurnActions.js
+++ src/model/newModel/FirstTurnActions.js
@@ -52,19 +52,16 @@ module.exports = function(UnitType, TerritoryType, Unit) {
         }
         const unitType =
           territory.type === earth ? UnitType.Legionnaire : UnitType.Ship;
-        const currentValue = playerTerritories
-          .map(territory => territory.getUnitsOfType(player, unitType).length)
-          .sum();
-        const allowedValue =
-          2 + (god.unitType && god.unitType === unitType ? 1 : 0);
-        if (currentValue === allowedValue) {
+        const unitsLeft = this.getUnitsLeft({ game });
+        const unitsOfTypeLeft = unitsLeft[unitType.id];
+        if (!unitsOfTypeLeft) {
           throw new Error(
             "vous ne pouvez pas ajouter d'autres unités de type " +
               unitType.label +
               "."
           );
         }
-        if (currentValue === allowedValue - 1 && territory.isOwner(player)) {
+        if (unitsOfTypeLeft === 1 && territory.isOwner(player)) {
           throw new Error(
             "vous devez prendre 2 territoires terrestres et 2 territoires maritimes contigus."
           );
@@ -83,6 +80,25 @@ module.exports = function(UnitType, TerritoryType, Unit) {
           "Il est impossible de placer une unité sur ce territoire : "
         );
       }
+    }
+    getUnitsLeft({ game }) {
+      const { player, god } = game.getCurrentPlayerAndGod();
+      const playerTerritories = game.getTerritoriesForPlayer(player);
+      const getAllowed = unitType => {
+        return 2 + (god.unitType && god.unitType === unitType ? 1 : 0);
+      };
+      const getLeft = unitType => {
+        const currentValue = playerTerritories
+          .map(territory => territory.getUnitsOfType(player, unitType).length)
+          .reduce((acc, value) => acc + value, 0);
+        return getAllowed(unitType) - currentValue;
+      };
+      const result = {};
+      const unitTypes = [UnitType.Legionnaire, UnitType.Ship];
+      unitTypes.forEach(type => {
+        result[type.id] = getLeft(type);
+      });
+      return result;
     }
   }
   return FirstTurnActions;
